184bc969ae9e8178483a93361400e9a978552fa2
ren522686239 <mrren888@163.com>
2018-02-24 15:02:01 +0800

    merge

---
+++ RMHBase/Classes/Macro/FunctionMacro.h
@@ -0,0 +1,16 @@
+//
+//  FunctionMacro.h
+//  SPL
+//
+//  Created by 任梦晗 on 2018/2/24.
+//  Copyright © 2018年 任梦晗. All rights reserved.
+//
+
+#ifndef FunctionMacro_h
+#define FunctionMacro_h
+#import "NSString+TT.h"
+
+/// 宏函数
+#define IsEmptyString(str)      (!str || [str.trim isEqualToString : @""])
+
+#endif /* FunctionMacro_h */
